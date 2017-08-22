7204cde10fbff70413b8b42edf9c2d31d1f9153d
Wen Zhang <iwenyou@gmail.com>
2017-08-22 16:54:10 -0700

    added redux-promise and applied to index.js"

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { provider} from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+
+import App from './App';
+
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
