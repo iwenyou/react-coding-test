7f4f62d048044390e59ba20a8adc05f5047954ba
Wen Zhang <iwenyou@gmail.com>
2017-08-22 16:45:34 -0700

    added on input change to search form

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -2,10 +2,29 @@ import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
 
+  constructor(props){
+    super(props);
+
+    this.state = {term:''};
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event){
+    this.setState({term: event.target.value});
+  }
+
+
+
   render(){
     return (
       <form className="input-group">
-        <input className="form-control"/>
+        <input
+          className="form-control"
+          placeholder="Enter search term"
+          value={this.state.term}
+          onChange={this.onInputChange}
+          />
         <span className="input-group-btn">
           <button className="btn btn-submit">
             submit
